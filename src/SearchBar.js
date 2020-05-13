718e234a34dce0d0f8fb1c8f872a872c2c7f6139
Nhan Thanh Tran <nhantt.bv@cybersoft.vn>
2020-05-13 20:42:03 +0700

    reduce background image 's size. add media querry

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -51,14 +51,6 @@ function SearchBar() {
           type="text"
           onChange={updaterecipeInput}
         ></input>
-        {/* <Select className="search-bar-input-select" value="Diet Label">
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select> */}
         <button>Ingredient</button>
       </div>
       <button type="submit">Search</button>
